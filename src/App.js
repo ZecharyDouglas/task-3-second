3c85f10df98c0d598c4e92dc52c86b65a816cf29
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-02-15 20:24:57 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import productNames from './componentfolder/productNames';
 import productsSorted from './componentfolder/sortedPrice';
 import totalCost from './componentfolder/totalCost';
+import onSale from './componentfolder/ProductsOnSale';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className='productsSorted'></div>
-      <div className='totalCost'></div>
-      <div className='sortedPrice'></div>
+        
+     
+       <div className='totalCost'></div>
+       <div className='productNames'></div>
+       <div className='sortedPrice'></div>
+       <div className='ProductsOnSale'></div>
+      
     </div>
   );
 }
--- src/componentfolder/totalCost.js
+++ src/componentfolder/totalCost.js
@@ -1,6 +1,7 @@
 import products from "../datafile";
 
-const totalCost = products.reduce((total,product)=> {return total += product},0);
+const totalCost = products.reduce((total,product)=> total += product.price,0);
+
 console.log(totalCost);
 
 export default totalCost;
